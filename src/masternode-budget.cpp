02a5bc675de7e4c22de582678463160085b03beb
Carbon Zero <42657652+Carbon-Zero@users.noreply.github.com>
2019-03-03 20:16:54 -0500

    Update masternode-budget.cpp

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -1023,7 +1023,7 @@ void CBudgetManager::NewBlock()
 
     if (strBudgetMode == "suggest") { //suggest the budget we see
         SubmitFinalBudget();
-    //}
+    }
 
     //this function should be called 1/14 blocks, allowing up to 100 votes per day on all proposals
     if (chainActive.Height() % 14 != 0) return;
