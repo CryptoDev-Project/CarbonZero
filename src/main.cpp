7539453dfc51a3bd0b193a348818ae6053ea9d61
Carbon Zero <42657652+Carbon-Zero@users.noreply.github.com>
2019-03-05 15:30:37 -0500

    Remove rejection for insane fees

--- src/main.cpp
+++ src/main.cpp
@@ -1428,10 +1428,10 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState& state, const CTransa
             }
         }
 
-        if (fRejectInsaneFee && nFees > ::minRelayTxFee.GetFee(nSize) * 10000)
-            return error("AcceptToMemoryPool: : insane fees %s, %d > %d",
-                hash.ToString(),
-                nFees, ::minRelayTxFee.GetFee(nSize) * 10000);
+        //if (fRejectInsaneFee && nFees > ::minRelayTxFee.GetFee(nSize) * 10000)
+        //    return error("AcceptToMemoryPool: : insane fees %s, %d > %d",
+        //        hash.ToString(),
+        //        nFees, ::minRelayTxFee.GetFee(nSize) * 10000);
 
         // Check against previous transactions
         // This is done last to help prevent CPU exhaustion denial-of-service attacks.
